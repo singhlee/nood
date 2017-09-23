4f290f4666e0e622d332f20ca025cc1298d41eaa
jackelee1990 <jackelee@foxmail.com>
2017-09-23 16:08:28 +0800

    删除nood-web

--- nood-config/src/main/java/org/nood/ConfigApplication.java
+++ nood-config/src/main/java/org/nood/ConfigApplication.java
@@ -2,6 +2,7 @@ package org.nood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
