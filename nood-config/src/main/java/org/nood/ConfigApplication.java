accb96b7a06677d232f59aece0436a2ef722a9dc
jackelee1990 <jackelee@foxmail.com>
2017-12-06 15:16:19 +0800

    新增系统管理服务

--- nood-config/src/main/java/org/nood/ConfigApplication.java
+++ nood-config/src/main/java/org/nood/ConfigApplication.java
@@ -5,20 +5,20 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@Configuration
-@EnableAutoConfiguration
 @EnableDiscoveryClient
 @EnableConfigServer
+@SpringBootApplication
 public class ConfigApplication
 {
     public static void main( String[] args )
 
     {
-        SpringApplication.run(ConfigApplication.class, args);
+        SpringApplication application = new SpringApplication(ConfigApplication.class);
+        ApplicationContext applicationContext = application.run(args);
+        System.out.println(applicationContext.getClass());
     }
 }
