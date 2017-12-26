d81d4b2b8afc60164ca5aede83ab85295631aa9e
singhlee <singhlee>
2017-12-26 14:18:56 +0800

    nood

--- nood-sys-web/src/main/java/org/nood/noodsysweb/controller/LoginController.java
+++ nood-sys-web/src/main/java/org/nood/noodsysweb/controller/LoginController.java
@@ -1,14 +1,12 @@
 package org.nood.noodsysweb.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
-    
-    private final static Logger logger = LoggerFactory.getLogger(LoginController.class);
+
 
 
     @RequestMapping("/")
