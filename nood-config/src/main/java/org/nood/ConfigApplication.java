14ad5a5a2406ee12f98448ec6bcd9b557304b727
hdxj_lx <hdxj_lx@mobilereality.org>
2019-12-28 17:55:40 +0800

    增加consul 服务注册

--- nood-config/src/main/java/org/nood/ConfigApplication.java
+++ nood-config/src/main/java/org/nood/ConfigApplication.java
@@ -1,16 +1,12 @@
 package org.nood;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RestController;
 
-
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigApplication
